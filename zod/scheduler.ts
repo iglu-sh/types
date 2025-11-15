cb52c1220b00fc4ba01d2125a468dfc89287a7b5
SirBerg <benno@boerg.co>
2025-11-15 20:31:27 +0100

    Introduced zod type

--- zod/scheduler.ts
+++ zod/scheduler.ts
@@ -1,6 +1,6 @@
 import {z} from "zod"
-import {valid_build_states as type_build_states} from "../core/db";
-const valid_build_states:type_build_states[] = ["created", "claimed", "starting", "running", "failed", "success", "aborted"]
+
+const valid_build_states:string[] = ["created", "claimed", "starting", "running", "failed", "success", "aborted"]
 export const controllerStateUpdateSchema = z.object({
     old_state: z.enum(valid_build_states),
     new_state: z.enum(valid_build_states),
